eb5f4df404aac4c46710b5ba8f2d424dcb4774be
Daniel Cueto <dcueto14.info@gmail.com>
2024-10-08 20:54:12 +0200

    Fixed some styles for text content and contrasts

--- src/components/shared/astro/mdxComponents/mdxComponents.ts
+++ src/components/shared/astro/mdxComponents/mdxComponents.ts
@@ -1,9 +1,11 @@
 import P from "./P.astro";
 // import A from "./A.astro";
 import H1 from "./H1.astro";
+import Image from "./Image.astro";
 
 export const mdxComponents = {
   p: P,
   // a: A,
-  h1: H1
+  h1: H1,
+  img: Image,
 }
